4626a23aef0558ffb254abcc50abf0cd0fa9d6a9
Mihlali Nonyukela <77958272+MihlaliNonyukela@users.noreply.github.com>
2025-01-29 19:41:03 +0200

    Update TowerOfHanoi.java

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -2,9 +2,7 @@
 
  
 /* 
-	This program implements a solution for the Towers of Hanoi problem.
-	A tower of 5 disks must be moved from rod A to rod C using B as a
-	spare or temporary storage space
+
 */
 
 public class TowerOfHanoi
